0e0e6c7e3948cc6a4220eb712b1972d38f00b4fe
suresh-guttikonda <guttikondasuresh7@gmail.com>
2021-04-29 11:35:56 +0200

    incorporated rgb_obs in NavigateGibsonEnv

--- src/rl_agents/test_suite_gibson.py
+++ src/rl_agents/test_suite_gibson.py
@@ -2,9 +2,11 @@
 from environments import suite_gibson
 import os
 
-suite_gibson.load(
+tf_env = suite_gibson.load(
     config_file=os.path.join('./configs', 'turtlebot_point_nav.yaml'),
     model_id=None,
     env_mode='headless',
     device_idx=0,
 )
+print('observation_spec', tf_env.time_step_spec().observation)
+print('action_spec', tf_env.action_spec())
