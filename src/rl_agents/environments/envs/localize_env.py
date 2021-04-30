1e99e62173a9458b71f49c32710924fe82ba3cde
suresh-guttikonda <guttikondasuresh7@gmail.com>
2021-04-30 20:59:36 +0200

    inprogress: pfnet_step() and pfnet_reset()

--- src/rl_agents/environments/envs/localize_env.py
+++ src/rl_agents/environments/envs/localize_env.py
@@ -42,6 +42,10 @@ class LocalizeGibsonEnv(iGibsonEnv):
                         render_to_tensor=render_to_tensor,
                         automatic_reset=automatic_reset)
 
+        # manually remove point navigation task termination and reward conditions
+        del self.task.termination_conditions[-1]
+        del self.task.reward_functions[-1]
+
         # override observation_space
         # task_obs_dim = robot_prorpio_state (18) + goal_coords (2)
         TASK_OBS_DIM = 18
@@ -65,6 +69,34 @@ class LocalizeGibsonEnv(iGibsonEnv):
         self.observation_space = gym.spaces.Dict(observation_space)
 
         self.pfnet_model = None
+
+    def load_miscellaneous_variables(self):
+        """
+        Load miscellaneous variables for book keeping
+        """
+
+        super(LocalizeGibsonEnv, self).load_miscellaneous_variables()
+
+        self.obstacle_map = None
+        self.floor_map = None
+
+        self.curr_pfnet_state = None
+        self.curr_rgb_obs = None
+        self.curr_gt_pose = None
+
+    def reset_variables(self):
+        """
+        Reset bookkeeping variables for the next new episode
+        """
+
+        super(LocalizeGibsonEnv, self).reset_variables()
+
+        self.obstacle_map = None
+        self.floor_map = None
+
+        self.curr_pfnet_state = None
+        self.curr_rgb_obs = None
+        self.curr_gt_pose = None
 
     def step(self, action):
         """
@@ -118,3 +150,119 @@ class LocalizeGibsonEnv(iGibsonEnv):
             custom_state['rgb'] = state['rgb']  # [0, 1] range rgb image
 
         return processed_state
+
+    def step_pfnet(self, new_rgb_obs):
+        """
+        """
+
+        old_rgb_obs = self.curr_rgb_obs
+        old_gt_pose = self.curr_gt_pose[0].numpy()
+        old_pfnet_state = self.curr_pfnet_state
+
+        # get new robot state
+        new_robot_state = self.robots[0].calc_state()
+
+        # process new rgb observation: convert to [-1, +1] range
+        new_rgb_obs = datautils.process_raw_image(new_rgb_obs)
+
+        # process new robot state: convert coords to pixel space
+        new_pose = self.get_robot_pose(robot_state, floor_map.shape)
+
+        # calculate actual odometry b/w old pose and new pose
+        assert list(old_pose.shape) == [3] and list(new_pose.shape) == [3]
+        new_odom = datautils.calc_odometry(old_pose, new_pose)
+
+        # convert to tensor
+        new_rgb_obs = tf.expand_dims(
+                    tf.convert_to_tensor(new_rgb_obs, dtype=tf.float32)
+                    , axis=0)
+        new_odom = tf.expand_dims(
+                    tf.convert_to_tensor(new_odom, dtype=tf.float32)
+                    , axis=0)
+        new_pose = tf.expand_dims(
+                        tf.convert_to_tensor(new_pose, dtype=tf.float32)
+                        , axis=0)
+
+        # construct pfnet input
+        odometry = tf.expand_dims(new_odom, axis=1)
+        observation = tf.expand_dims(old_rgb_obs, axis=1)
+        input = [observation, odometry]
+        model_input = (input, old_pfnet_state)
+
+
+        """
+        ## HACK:
+            if stateful: reset RNN s.t. initial_state is set to initial particles and weights
+                start of each trajectory
+            if non-stateful: pass the state explicity every step
+        """
+        if self.stateful:
+            self.pfnet_model.layers[-1].reset_states(old_pfnet_state)    # RNN layer
+
+        # forward pass pfnet
+        # output: contains particles and weights before transition update
+        # pfnet_state: contains particles and weights after transition update
+        output, new_pfnet_state = self.pfnet_model(model_input, training=False)
+
+        # compute pfnet loss
+        particles, particle_weights = output
+        true_old_pose = tf.expand_dims(self.curr_gt_pose, axis=1)
+        loss_dict = pfnet_loss.compute_loss(particles, particle_weights, true_old_pose, self.map_pixel_in_meters)
+
+        ## TODO: may need better reward
+        # compute reward
+        reward = reward - tf.squeeze(loss_dict['coords']).numpy()
+
+        self.curr_pfnet_state = new_pfnet_state
+        self.curr_gt_pose = new_pose
+        self.curr_rgb_obs = new_rgb_obs
+
+    def reset_pfnet(self, new_rgb_obs):
+        """
+        """
+
+        # get new robot state
+        new_robot_state = self.robots[0].calc_state()
+
+        # process new rgb observation: convert to [-1, +1] range
+        new_rgb_obs = datautils.process_raw_image(new_rgb_obs)
+
+        # process new robot state: convert coords to pixel space
+        new_pose = self.get_robot_pose(robot_state, floor_map.shape)
+
+        # process new env map
+        new_floor_map = self.get_floor_map()
+        new_obstacle_map = self.get_obstacle_map()
+
+        # get random particles based on init distribution conditions
+        rnd_particles = self.get_random_particles(
+                                num_particles,
+                                particles_distr,
+                                true_pose.numpy(),
+                                floor_map[0],
+                                particles_cov)
+
+        # convert to tensor
+        new_rgb_obs = tf.expand_dims(
+                    tf.convert_to_tensor(new_rgb_obs, dtype=tf.float32)
+                    , axis=0)
+        new_pose = tf.expand_dims(
+                        tf.convert_to_tensor(new_pose, dtype=tf.float32)
+                        , axis=0)
+        new_floor_map = tf.expand_dims(
+                    tf.convert_to_tensor(new_floor_map, dtype=tf.float32)
+                    , axis=0)
+        new_obstacle_map = tf.expand_dims(
+                    tf.convert_to_tensor(new_obstacle_map, dtype=tf.float32)
+                    , axis=0)
+        new_init_particles = tf.convert_to_tensor(rnd_particles, dtype=tf.float32)
+        new_init_particle_weights = tf.constant(
+                                    np.log(1.0/float(num_particles)),
+                                    shape=(batch_size, num_particles),
+                                    dtype=tf.float32)
+
+        self.obstacle_map = obstacle_map
+        self.floor_map = floor_map
+        self.curr_pfnet_state = [init_particles, init_particle_weights, obstacle_map]
+        self.curr_gt_pose = new_pose
+        self.curr_rgb_obs = new_rgb_obs
