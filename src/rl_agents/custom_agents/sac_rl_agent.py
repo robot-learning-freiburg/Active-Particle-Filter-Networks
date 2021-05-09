2cd54bac34224b533302c03a791bae77b46a90bc
suresh-guttikonda <guttikondasuresh7@gmail.com>
2021-05-09 08:53:16 +0200

    base code for sac agent

--- src/rl_agents/custom_agents/sac_rl_agent.py
+++ src/rl_agents/custom_agents/sac_rl_agent.py
@@ -6,7 +6,9 @@ import os
 import tensorflow as tf
 
 # import custom tf_agents
-from tf_agents.agents.ppo import ppo_clip_agent
+from tf_agents.agents.ddpg import critic_network
+from tf_agents.agents.sac import sac_agent
+from tf_agents.agents.sac import tanh_normal_projection_network
 from tf_agents.environments import py_environment
 from tf_agents.networks import actor_distribution_network
 from tf_agents.networks import value_network
@@ -16,9 +18,9 @@ from tf_agents.networks.utils import mlp_layers
 from tf_agents.train.utils import spec_utils
 
 
-class PPOAgent(object):
+class SACAgent(object):
     """
-    Custom PPO Agent implementation for iGibsonEnv
+    Custom SAC Agent implementation for iGibsonEnv
     """
 
     def __init__(self,
@@ -29,8 +31,15 @@ class PPOAgent(object):
                  gpu=0,
                  use_tf_function=True,
                  # train
-                 num_epochs=25,
-                 learning_rate=1e-3,
+                 actor_learning_rate=3e-4,
+                 critic_learning_rate=3e-4,
+                 alpha_learning_rate=3e-4,
+                 target_update_tau=0.005,
+                 target_update_period=1,
+                 td_errors_loss_fn=tf.math.squared_difference,
+                 gamma=0.99,
+                 reward_scale_factor = 1.0,
+                 gradient_clipping=None,
                  debug_summaries=False,
                  summarize_grads_and_vars=False,
                  ):
@@ -49,10 +58,24 @@ class PPOAgent(object):
             gpu device number
         :param use_tf_function: bool
             whether or not wrap function into tf graph
-        :param num_epochs: int
-            number of epochs for computing policy updates
-        :param learning_rate: float
+        :param actor_learning_rate: float
+            learning rate of optimizer used for actor
+        :param critic_learning_rate: float
+            learning rate of optimizer used for critic
+        :param alpha_learning_rate: float
             learning rate of optimizer used for agent
+        :param target_update_tau: float
+            factor for soft update
+        :param target_update_period: int
+            period for soft update
+        :param td_errors_loss_fn:
+            function to compute element wise loss
+        :param gamma:
+            reward discount factor
+        :param reward_scale_factor:
+            scale factor for reward
+        :param gradient_clipping:
+            to clip gradients
         :param debug_summaries: bool
             whether or not to gather debug summaries
         :param summarize_grads_and_vars: bool
@@ -82,7 +105,9 @@ class PPOAgent(object):
             self.__conv_2d_layer_params = [(32, (8, 8), 4), (64, (4, 4), 2), (64, (3, 3), 2)]
             self.__encoder_fc_layers = [256]
             self.__actor_fc_layers = [256]
-            self.__value_fc_layers = [256]
+            self.__critic_obs_fc_layers = [256]
+            self.__critic_action_fc_layers = [256]
+            self.__critic_joint_fc_layers = [256]
 
         with strategy.scope():
             # instantiate preprocessing_layers and preprocessing_combiner
@@ -98,34 +123,47 @@ class PPOAgent(object):
                 preprocessing_layers=preprocessing_layers,
                 preprocessing_combiner=preprocessing_combiner,
                 fc_layer_params=self.__actor_fc_layers,
-                activation_fn=tf.keras.activations.tanh,
+                # continuous_projection_net=tanh_normal_projection_network.TanhNormalProjectionNetwork,
+                kernel_initializer=self.__glorot_uniform_initializer,
             )
 
         with strategy.scope():
             # instantiate value network to estimate V(s)
-            value_net = value_network.ValueNetwork(
-                input_tensor_spec=observation_spec,
+            critic_net = critic_network.CriticNetwork(
+                input_tensor_spec=(
+                    observation_spec,
+                    action_spec
+                ),
                 preprocessing_layers=preprocessing_layers,
                 preprocessing_combiner=preprocessing_combiner,
-                fc_layer_params=self.__value_fc_layers,
-                activation_fn=tf.keras.activations.tanh,
+                observation_fc_layer_params=self.__critic_obs_fc_layers,
+                action_fc_layer_params=self.__critic_action_fc_layers,
+                joint_fc_layer_params=self.__critic_joint_fc_layers,
+                kernel_initializer=self.__glorot_uniform_initializer,
             )
 
         with strategy.scope():
-            # instantiate the ppo agent
-            tf_agent = ppo_clip_agent.PPOClipAgent(
+            # instantiate the sac agent
+            tf_agent = sac_agent.SacAgent(
                 time_step_spec=time_step_spec,
                 action_spec=action_spec,
-                optimizer=tf.compat.v1.train.AdamOptimizer(
-                    learning_rate=learning_rate
+                critic_network=critic_net,
+                actor_network=actor_net,
+                actor_optimizer=tf.compat.v1.train.AdamOptimizer(
+                    learning_rate=actor_learning_rate
                 ),
-                actor_net=actor_net,
-                value_net=value_net,
-                importance_ratio_clipping=0.2,
-                normalize_observations=False,
-                normalize_rewards=False,
-                use_gae=True,
-                num_epochs=num_epochs,
+                critic_optimizer=tf.compat.v1.train.AdamOptimizer(
+                    learning_rate=critic_learning_rate
+                ),
+                alpha_optimizer=tf.compat.v1.train.AdamOptimizer(
+                    learning_rate=alpha_learning_rate
+                ),
+                target_update_tau=target_update_tau,
+                target_update_period=target_update_period,
+                td_errors_loss_fn=td_errors_loss_fn,
+                gamma=gamma,
+                reward_scale_factor=reward_scale_factor,
+                gradient_clipping=gradient_clipping,
                 debug_summaries=debug_summaries,
                 summarize_grads_and_vars=summarize_grads_and_vars,
                 train_step_counter=train_step_counter,
