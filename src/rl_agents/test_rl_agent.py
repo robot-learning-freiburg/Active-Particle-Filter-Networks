07b783add57816f4d9e969e4bbf32435487c225e
suresh-guttikonda <guttikondasuresh7@gmail.com>
2021-05-21 07:04:30 +0200

    use tf_agents actor to collect init replay buffer samples

--- src/rl_agents/test_rl_agent.py
+++ src/rl_agents/test_rl_agent.py
@@ -8,6 +8,7 @@ import tensorflow as tf
 # import custom tf_agents
 from environments import suite_gibson
 from tf_agents.policies import random_py_policy
+from tf_agents.train import actor
 
 
 def main():
@@ -17,16 +18,18 @@ def main():
         'turtlebot_point_nav.yaml'
     )
     is_localize_env = True
+    use_tf_function = True
     action_timestep = 1.0 / 10.0
     physics_timestep = 1.0 / 40.0
     device_idx = 0
+    initial_collect_steps = 100
 
     # create igibson env
     py_env = suite_gibson.load(
         config_file=config_file,
         model_id=None,
         env_mode='headless',
-        use_tf_function=True,
+        use_tf_function=use_tf_function,
         is_localize_env=is_localize_env,
         action_timestep=action_timestep,
         physics_timestep=physics_timestep,
@@ -39,13 +42,24 @@ def main():
         action_spec=py_env.action_spec()
     )
 
-    time_step = py_env.reset()
-    while not time_step.is_last():
-        py_env.render('human')
-        action_step = random_policy.action(time_step)
-        time_step = py_env.step(action_step.action)
-        print(time_step.reward)
-    py_env.close()
+    # use random policy to collect initial experiences to seed the replay buffer
+    initial_collect_actor = actor.Actor(
+        env=py_env,
+        policy=random_policy,
+        train_step=0,
+        steps_per_run=initial_collect_steps,
+        observers=[],
+        metrics=None,
+    )
+    initial_collect_actor.run()
+
+    # time_step = py_env.reset()
+    # while not time_step.is_last():
+    #     py_env.render('human')
+    #     action_step = random_policy.action(time_step)
+    #     time_step = py_env.step(action_step.action)
+    #     print(time_step.reward)
+    # py_env.close()
 
     print('done')
 
@@ -62,4 +76,7 @@ if __name__ == '__main__':
     np.random.seed(seed)
     tf.random.set_seed(seed)
 
+    logdir = './test_output'
+    tf.profiler.experimental.start(logdir=logdir)
     main()
+    tf.profiler.experimental.stop()
