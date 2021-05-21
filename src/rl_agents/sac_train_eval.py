fc8ac0f2297cb16fce304717320c6ad01aa77d3b
suresh-guttikonda <guttikondasuresh7@gmail.com>
2021-05-21 08:50:31 +0200

    further logging changes

--- src/rl_agents/sac_train_eval.py
+++ src/rl_agents/sac_train_eval.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
+from absl import app
+from absl import flags
 from absl import logging
-import argparse
 import numpy as np
 import os
 import random
@@ -17,139 +18,106 @@ from tf_agents.train import triggers
 from tf_agents.train.utils import strategy_utils
 from tf_agents.utils import common
 
+flags.DEFINE_string(
+    name='root_dir',
+    default='./test_output',
+    help='Root directory for writing logs/summaries/checkpoints.'
+)
 
-def parse_args():
-    """
-    Parse command line arguments
+# define training parameters
+flags.DEFINE_integer(
+    name='replay_buffer_capacity',
+    default=1000,
+    help='Replay buffer capacity'
+)
+flags.DEFINE_integer(
+    name='sequence_length',
+    default=2,
+    help='Consecutive sequence length'
+)
+flags.DEFINE_integer(
+    name='stride_length',
+    default=1,
+    help='Sliding window stride'
+)
+flags.DEFINE_integer(
+    name='batch_size',
+    default=1,
+    help='Batch size for each training step'
+)
+flags.DEFINE_integer(
+    name='initial_collect_steps',
+    default=100,
+    help='Number of steps to collect at the beginning of training using random policy'
+)
+flags.DEFINE_integer(
+    name='num_eval_episodes',
+    default=1,
+    help='Number of episodes to run evaluation'
+)
+flags.DEFINE_integer(
+    name='num_iterations',
+    default=3000,
+    help='Total number train/eval iterations to perform'
+)
+flags.DEFINE_integer(
+    name='eval_interval',
+    default=500,
+    help='Run evaluation every eval_interval train steps'
+)
+flags.DEFINE_integer(
+    name='log_interval',
+    default=100,
+    help='Log loss every log_interval train steps'
+)
+flags.DEFINE_integer(
+    name='policy_save_interval',
+    default=500,
+    help='Save policies every policy_save_interval train steps'
+)
+flags.DEFINE_boolean(
+    name='use_tf_function',
+    default=True,
+    help='Whether to use graph/eager mode execution'
+)
+flags.DEFINE_integer(
+    name='seed',
+    default=100,
+    help='Fix the random seed'
+)
 
-    :return: argparse.Namespace
-        parsed command-line arguments passed to *.py
-    """
+# define igibson env parameters
+flags.DEFINE_boolean(
+    name='is_localize_env',
+    default=True,
+    help='Whether to use navigation/localization env'
+)
+flags.DEFINE_string(
+    name='config_file',
+    default=os.path.join(
+        os.path.dirname(os.path.realpath(__file__)),
+        'configs',
+        'turtlebot_point_nav.yaml'
+    ),
+    help='Config file for the experiment'
+)
+flags.DEFINE_float(
+    name='action_timestep',
+    default=1.0 / 10.0,
+    help='Action time step for the simulator'
+)
+flags.DEFINE_float(
+    name='physics_timestep',
+    default=1.0 / 40.0,
+    help='Physics time step for the simulator'
+)
+flags.DEFINE_integer(
+    name='gpu_num',
+    default=0,
+    help='GPU id for graphics/computation'
+)
 
-    # initialize parser
-    arg_parser = argparse.ArgumentParser()
-
-    # define training parameters
-    arg_parser.add_argument(
-        '--replay_buffer_capacity',
-        type=int,
-        default=1000,
-        help='Replay buffer capacity'
-    )
-    arg_parser.add_argument(
-        '--sequence_length',
-        type=int,
-        default=2,
-        help='Consecutive sequence length'
-    )
-    arg_parser.add_argument(
-        '--stride_length',
-        type=int,
-        default=1,
-        help='Sliding window stride'
-    )
-    arg_parser.add_argument(
-        '--batch_size',
-        type=int,
-        default=8,
-        help='Batch size for each training step'
-    )
-    arg_parser.add_argument(
-        '--initial_collect_steps',
-        type=int,
-        default=100,
-        help='Number of steps to collect at the beginning of training using random policy'
-    )
-    arg_parser.add_argument(
-        '--num_eval_episodes',
-        type=int,
-        default=5,
-        help='Number of episodes to run evaluation'
-    )
-    arg_parser.add_argument(
-        '--num_iterations',
-        type=int,
-        default=3000,
-        help='Total number train/eval iterations to perform'
-    )
-    arg_parser.add_argument(
-        '--eval_interval',
-        type=int,
-        default=500,
-        help='Run evaluation every eval_interval train steps'
-    )
-    arg_parser.add_argument(
-        '--log_interval',
-        type=int,
-        default=100,
-        help='Log loss every log_interval train steps'
-    )
-    arg_parser.add_argument(
-        '--policy_save_interval',
-        type=int,
-        default=500,
-        help='Save policies every policy_save_interval train steps'
-    )
-    arg_parser.add_argument(
-        '--seed',
-        type=int,
-        default=100,
-        help='Fix the random seed'
-    )
-
-    # define igibson env parameters
-    arg_parser.add_argument(
-        '--config_file',
-        type=str,
-        default=os.path.join(
-            os.path.dirname(os.path.realpath(__file__)),
-            'configs',
-            'turtlebot_point_nav.yaml'
-        ),
-        help='Config file for the experiment'
-    )
-    arg_parser.add_argument(
-        '--action_timestep',
-        type=float,
-        default=1.0 / 10.0,
-        help='Action time step for the simulator'
-    )
-    arg_parser.add_argument(
-        '--physics_timestep',
-        type=float,
-        default=1.0 / 40.0,
-        help='Physics time step for the simulator'
-    )
-    arg_parser.add_argument(
-        '--gpu_num',
-        type=int,
-        default=0,
-        help='GPU id for graphics/computation'
-    )
-
-    # parse parameters
-    params = arg_parser.parse_args()
-
-    # post-processing
-    params.root_dir = './test_output'
-    params.is_localize_env = False
-    params.summary_interval = 1000
-    params.use_tf_function = True
-
-    # set random seeds
-    random.seed(params.seed)
-    np.random.seed(params.seed)
-    tf.random.set_seed(params.seed)
-
-    os.environ['CUDA_VISIBLE_DEVICES'] = str(params.gpu_num)
-    os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = 'true'
-
-    logging.set_verbosity(logging.INFO)
-    tf.compat.v1.enable_v2_behavior()
-    tf.debugging.enable_check_numerics()  # error out inf or NaN
-
-    logging.info(params)
-    return params
+FLAGS = flags.FLAGS
 
 
 def get_eval_metrics(eval_actor):
@@ -204,8 +172,8 @@ def train_eval(arg_params):
         # create or get global step tensor
         global_step = tf.compat.v1.train.get_or_create_global_step()
 
-    logging.info('Creating SAC Agent')
     # create sac agent
+    logging.info('Creating SAC Agent')
     sac_agent = SACAgent(
         root_dir=arg_params.root_dir,
         env_load_fn=lambda model_id, mode, use_tf_function, device_idx: suite_gibson.load(
@@ -329,6 +297,7 @@ def train_eval(arg_params):
     for _ in range(arg_params.num_iterations):
         # training
         # Creates a trace event for each training step with the step number.
+        step = agent_learner.train_step_numpy
         with tf.profiler.experimental.Trace("Train", step_num=step):
             collect_actor.run()
             loss_info = agent_learner.run(
@@ -361,6 +330,26 @@ def train_eval(arg_params):
     tf.profiler.experimental.stop()
 
 
+def main(_):
+    logging.set_verbosity(logging.INFO)
+    tf.compat.v1.enable_v2_behavior()
+    tf.debugging.enable_check_numerics()  # error out inf or NaN
+
+    os.environ['CUDA_VISIBLE_DEVICES'] = str(FLAGS.gpu_num)
+    os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = 'true'
+
+    print('==================================================')
+    for k, v in FLAGS.flag_values_dict().items():
+        print(k, v)
+    print('==================================================')
+
+    # set random seeds
+    random.seed(FLAGS.seed)
+    np.random.seed(FLAGS.seed)
+    tf.random.set_seed(FLAGS.seed)
+
+    train_eval(FLAGS)
+
+
 if __name__ == '__main__':
-    parsed_params = parse_args()
-    train_eval(parsed_params)
+    app.run(main)
