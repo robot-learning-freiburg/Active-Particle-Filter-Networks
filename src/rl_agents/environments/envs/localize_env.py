60cfb102c6d1143e65353790ae2aa7928c00fa82
suresh-guttikonda <guttikondasuresh7@gmail.com>
2021-09-11 14:34:13 +0200

    lower update freq

--- src/rl_agents/environments/envs/localize_env.py
+++ src/rl_agents/environments/envs/localize_env.py
@@ -180,6 +180,7 @@ class LocalizeGibsonEnv(iGibsonEnv):
         self.pf_params.global_map_size = np.array(FLAGS.global_map_size, dtype=np.int32)
         self.pf_params.window_scaler = FLAGS.window_scaler
         self.pf_params.max_step = self.config.get('max_step', 500)
+        self.pf_params.loop = 6
 
         self.pf_params.transition_std[0] = (self.pf_params.transition_std[0] / self.map_pixel_in_meters) * self.trav_map_resolution # convert meters to pixels and rescale to trav map resolution
         self.pf_params.init_particles_std[0] = (self.pf_params.init_particles_std[0] / self.map_pixel_in_meters) * self.trav_map_resolution  # convert meters to pixels and rescale to trav map resolution
