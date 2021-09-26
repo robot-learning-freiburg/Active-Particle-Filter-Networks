101213a2be5645c00fd0ac3e7fe0ad9d745e8682
suresh-guttikonda <guttikondasuresh7@gmail.com>
2021-09-26 15:56:32 +0200

    plotting changes

--- src/plotting/plot.py
+++ src/plotting/plot.py
@@ -103,7 +103,7 @@ def house3d_plts():
     fig = plt.figure(figsize=(18, 12))
     ax = fig.add_subplot(111)
 
-    # pfnet house3d apartments generalize
+    # pfnet house3d apartment
     pfnet_train_path = "/media/neo/robotics/final_report/House3D_4000_8.0/house3d_rgb_depth/train/events.out.tfevents.1631127344.rlgpu2.10177.0.v2"
     pfnet_train_loss = np.array(getEventFileData(pfnet_train_path)["loss"])
     pfnet_eval_path = "/media/neo/robotics/final_report/House3D_4000_8.0/house3d_rgb_depth/eval/events.out.tfevents.1631127344.rlgpu2.10177.1.v2"
@@ -111,6 +111,7 @@ def house3d_plts():
     pfnet_train = ax.plot(pfnet_train_loss[:, 0], pfnet_train_loss[:, 1])
     pfnet_eval = ax.plot(pfnet_eval_loss[:, 0], pfnet_eval_loss[:, 1])
 
+    # dpf house3d apartment
     dpf_train_path = "/media/neo/robotics/deep-activate-localization/bckp/jan/jan_22/runs/Jan23_00-10-06_pearl8/train_stats_mean_total_loss/events.out.tfevents.1611357667.pearl8.6887.3"
     dpf_train_loss = np.array(getEventFileData(dpf_train_path)["train_stats"])
     dpf_eval_path = "/media/neo/robotics/deep-activate-localization/bckp/jan/jan_27_1/runs/Jan27_10-55-58_pearl8/eval_stats_mean_loss/events.out.tfevents.1611741820.pearl8.17432.3"
@@ -131,6 +132,30 @@ def house3d_plts():
     plt.show()
     fig.savefig("house3d_loss.png")
 
+def igibson_plts():
+    fig = plt.figure(figsize=(18, 12))
+    ax = fig.add_subplot(111)
+
+    N = np.array([500, 500, 1000])
+    T = np.array([50, 100, 100])
+    accuracy = np.array([90.0, 90.625, 91.875])
+    area = np.array([100, 200, 300])
+    colors = np.random.rand(len(N+1))
+
+    ax.scatter(x=N, y=T, s=area, c=colors, alpha=0.5)
+    for i, txt in enumerate(accuracy):
+        ax.annotate(f"    {txt}", (N[i], T[i]))
+
+    ax.set_xticks(np.array([0, 250, 500, 1000]))
+    ax.set_yticks(np.array([0, 10, 50, 100]))
+    ax.set_title('iGibson PFNet global localization RGB-D accuracy ', fontsize=18, weight='bold')
+    ax.set_xlabel("number of particles (N)", fontsize=16)
+    ax.set_ylabel("episode steps (t)", fontsize=16)
+
+    plt.show()
+    fig.savefig("igibson_rgbd_accuracy.png")
+
 if __name__ == '__main__':
     # generalization_plts()
-    house3d_plts()
+    # house3d_plts()
+    igibson_plts()
